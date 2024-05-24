da44bf32fc3f92bbe84c7731c7753ff89df5e897
William Jackson <lila.lilajackson@hotmail.com>
2024-05-24 13:34:55 -0300

    CSS do Form de POST

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import postBill from "../../api/postBill";
-
+import './form.css';
 const Form = () => {
     const [title, setTitle] = useState('');
     const [value, setValue] = useState(0);
@@ -22,23 +22,31 @@ const Form = () => {
         <>
             <h1>Crie sua conta</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Título</label>
-                <input type="text" placeholder="Título da sua conta" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <form onSubmit={handleSubmit} className="form-container">
+                <div className="field-wrapper">
+                    <label htmlFor="">Título</label>
+                    <input type="text" placeholder="Título da sua conta" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </div>
 
-                <label htmlFor="">Valor</label>
-                <input type="number" placeholder="Valor da sua conta" value={value} onChange={(e) => setValue(e.target.value)} />
+                <div className="field-wrapper">
+                    <label htmlFor="">Valor</label>
+                    <input type="number" placeholder="Valor da sua conta" value={value} onChange={(e) => setValue(e.target.value)} />
+                </div>
 
-                <label htmlFor="">Descrição</label>
-                <input type="text" placeholder="Descrição da sua conta" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <div className="field-wrapper">
+                    <label htmlFor="">Descrição</label>
+                    <input type="text" placeholder="Descrição da sua conta" value={description} onChange={(e) => setDescription(e.target.value)} />
+                </div>
 
-                <label htmlFor="">Pago</label>
-                <select value={isPaid} onChange={(e) => setIsPaid(e.target.value)}>
-                    <option value="false">Não</option>
-                    <option value="true">Sim</option>
-                </select>
+                <div className="field-wrapper">
+                    <label htmlFor="">Pago</label>
+                    <select value={isPaid} onChange={(e) => setIsPaid(e.target.value)} className="select-input">
+                        <option value="false">Não</option>
+                        <option value="true">Sim</option>
+                    </select>
+                </div>
 
-                <button type="submit">Confirmar alterações</button>
+                <button type="submit" className="btn-submit">Confirmar alterações</button>
             </form>
         </>
     )
