b9ebca111e9ef8c6ef5015d282648ee600bf7472
William Jackson <lila.lilajackson@hotmail.com>
2024-05-24 08:31:17 -0300

    create PUT

---
+++ src/api/putBill.js
@@ -0,0 +1,16 @@
+const putBill = (bill) => {
+
+    const url = "http://localhost:8080/bills/update";
+
+    try {
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(bill),
+        });
+    } catch (e) { console.error(e) }
+}
+
+export default putBill;
